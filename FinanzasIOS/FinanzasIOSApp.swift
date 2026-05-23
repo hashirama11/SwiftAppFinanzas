c5be55158a6a9bac8d23fdd7d12ef6cfb73f4ae4
Kevin Pastran <chesterearial@icloud.com>
2026-05-23 19:33:06 -0400

    Initial Commit

---
+++ FinanzasIOS/FinanzasIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanzasIOSApp.swift
+//  FinanzasIOS
+//
+//  Created by chester on 23/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanzasIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
